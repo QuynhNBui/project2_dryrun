9405535681fe6cdba7de6b2feee4f6fd39a40896
QuynhNBui <buiqnn@gmail.com>
2019-09-20 20:14:05 -0700

    commit

---
+++ static/js/logic.js
@@ -0,0 +1,94 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [37.773972, -122.431297],
+  zoom: 11
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Link to GeoJSON
+// var APILink = "http://data.beta.nyc//dataset/d6ffa9a4-c598-4b18-8caf-14abde6a5755/resource/74cdcc33-512f-439c-" +
+// "a43e-c09588c4b391/download/60dbe69bcd3640d5bedde86d69ba7666geojsonmedianhouseholdincomecensustract.geojson";
+// console.log(APILink);
+var geojson;
+
+//
+let filePath = "../geojsons/neighborhood_polygon_rates.geojson"
+
+// Grab data with d3
+d3.json(filePath, function(data) {
+
+  // Create a new choropleth layer
+  geojson = L.choropleth(data, {
+
+    // Define what  property in the features to use
+    valueProperty: "MRI",
+
+    // Set color scale
+    scale: ["#ffffb2", "#b10026"],
+
+    // Number of breaks in step range
+    steps: 10,
+
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+
+      layer.bindPopup("<h3>" + feature.properties.name +
+            "</h3><hr><p>Mortgage Rate Increase (%): " + feature.properties.MRI + "</p>").on({
+            mouseover: function() {
+                this.openPopup()
+            },
+            mouseout: function() {
+              this.closePopup()
+            },
+            click: function() {
+              this.openPopup()
+            }
+        });
+    }
+  }).addTo(myMap);
+
+  // Set up the legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h1>Median Income</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding legend to the map
+  legend.addTo(myMap);
+
+});
